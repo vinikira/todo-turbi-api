67655e39bf9c42dae20d9675787f5cf348f9ddb7
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-05-13 21:22:31 -0300

    add firebase service and made retrieve, retrieve one and create handlers. (missing git add .)

--- src/handlers/task.js
+++ src/handlers/task.js
@@ -1,7 +1,61 @@
-const get = (req, res) => {
-  res.json({id: 1, details: 'teste', schedule: new Date(), done: false})
+const { db } = require('../services/firebase/')
+
+const retrieve = (req, res) => {
+  const { user, filter } = req.query
+  const ref = db.ref('tasks')
+
+  ref.once('value', (snap) => res.json(snap.val()))
+}
+
+const retrieveOne = (req, res) => {
+  const { id } = req.params
+  const ref = db.ref(`tasks/${id}`)
+
+  ref.once('value', (snap) => res.json(snap.val()))
+}
+
+const create = (req, res) => {
+  const { name, detail, scheduled, user } = req.body
+
+  const resp = db.ref('tasks').push({
+    name,
+    detail,
+    scheduled,
+    user,
+    done: false
+  })
+
+  console.log(resp)
+
+  res.json({ success: true, id: resp.key })
+}
+
+const update = (req, res) => {
+  const { body, params } = req
+  const { name, detail, scheduled, done } = body
+  const { id } = params
+  const ref = db.ref(`tasks/${id}`)
+
+  res.json({ success: true, id: ref.key })
+}
+
+const makeDone = (req, res) => {
+  req.body.done = true
+
+  update(req, res)
+}
+
+const makeUnDone = (req, res) => {
+  req.body.done = false
+
+  update(req, res)
 }
 
 module.exports = {
-  get
+  retrieve,
+  retrieveOne,
+  create,
+  update,
+  makeDone,
+  makeUnDone
 }
--- src/routes/task.js
+++ src/routes/task.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { task } = require('../handlers')
-const { get } = task
+const { retrieve, retrieveOne, update, create, makeDone, makeUnDone } = task
 
-router.get('/tasks', get)
+router.get('/tasks', retrieve)
+router.get('/tasks/:id', retrieveOne)
+router.post('/tasks/', create)
+router.put('/tasks/:id', update)
+router.put('/tasks/:id/done', makeDone)
+router.put('/tasks/:id/undone', makeUnDone)
 
 module.exports = router
